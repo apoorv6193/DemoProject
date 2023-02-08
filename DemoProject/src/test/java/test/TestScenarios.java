a88451d61cbade82c6e726f995d072ed1f4ae66c
Apoorv Patel <apoorv6193@gmail.com>
2023-02-08 21:49:12 +0530

    Added wicketkeeper test

--- DemoProject/src/test/java/test/TestScenarios.java
+++ DemoProject/src/test/java/test/TestScenarios.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import constants.TestConstants;
 import utils.JsonDataReader;
 
 public class TestScenarios {
@@ -19,22 +21,33 @@ public class TestScenarios {
 		jsonDataReader = new JsonDataReader();
 	}
 
-	@Test
+	@Test(description = "Test to validate team has only 4 foreign players")
 	public void validateForeignPlayers() throws IOException, ParseException {
-		int fp = 0;
+		int foreignPlayerCount = 0;
 		JSONArray playerDetails = (JSONArray) jsonDataReader.getJsonData().get("player");
 		for (int i = 0; i < playerDetails.size(); i++) {
 			JSONObject player = (JSONObject) playerDetails.get(i);
-			if (!player.get("country").equals("India")) {
-				fp++;
+			if (!player.get(TestConstants.PLAYER_COUNTRY).equals("India")) {
+				foreignPlayerCount++;
 			}
 		}
-		System.out.println("Number of foreign players-- " + fp);
+		Assert.assertEquals(foreignPlayerCount, 4, "Team does not have 4 foreign players");
 	}
 
-	@Test
-	public void validateWicketKeepers() {
-
+	@Test(description = "Test to validate team has atleast 1 wicket keeper")
+	public void validateWicketKeepers() throws IOException, ParseException {
+		int wicketKeeper = 0;
+		JSONArray playerDetails = (JSONArray) jsonDataReader.getJsonData().get("player");
+		for (int i = 0; i < playerDetails.size(); i++) {
+			JSONObject player = (JSONObject) playerDetails.get(i);
+			if (player.get(TestConstants.PLAYER_ROLE).equals("Wicket-keeper")) {
+				wicketKeeper++;
+			}
+		}
+		if (wicketKeeper >= 1) {
+			Assert.assertTrue(true);
+		} else {
+			Assert.assertTrue(false, "Team does not have any wicket keeper");
+		}
 	}
-
 }
